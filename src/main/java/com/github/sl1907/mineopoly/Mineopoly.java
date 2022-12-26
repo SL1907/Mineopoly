3baa9d509e485eff0a8c2f62d14809b3554e4d3d
SL1907 <samuel.lennard@icloud.com>
2022-12-26 00:54:15 +0000

    Add command to test JSON structure

--- src/main/java/com/github/sl1907/mineopoly/Mineopoly.java
+++ src/main/java/com/github/sl1907/mineopoly/Mineopoly.java
@@ -1,8 +1,19 @@
 package com.github.sl1907.mineopoly;
 
+import com.github.sl1907.mineopoly.structure.MineopolySet;
+import com.github.sl1907.mineopoly.structure.tiles.MineopolyProperty;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import me.lucko.helper.Commands;
+import net.kyori.adventure.text.Component;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Arrays;
 
 public final class Mineopoly extends JavaPlugin {
 
@@ -10,11 +21,40 @@ public final class Mineopoly extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        Commands.create()
+                .assertPlayer()
+                .handler(c -> {
+                    URL resource = this.getClass().getResource("london.json");
+                    if (resource != null) {
+                        MineopolySet mineopolySet = this.readJson(resource);
+                        if (mineopolySet == null) {
+                            return;
+                        }
 
+                        for (MineopolyProperty property : mineopolySet.getProperties()) {
+                            c.sender().sendMessage(Component.text(
+                                    "%s | %s %s".formatted(property.getId(), property.getName(), Arrays.toString(property.getRent()))
+                            ));
+                        }
+                    }
+                })
+                .register("mineopoly");
+    }
+
+    private MineopolySet readJson(URL path) {
+        try {
+            File file = new File(path.toURI());
+            try (FileReader fileReader = new FileReader(file)) {
+                return GSON.fromJson(fileReader, MineopolySet.class);
+            } catch (IOException e) {
+                return null;
+            }
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 }
