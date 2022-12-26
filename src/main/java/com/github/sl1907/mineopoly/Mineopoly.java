6e373e9b33eae7b42504712404ff0671d3ac03d2
SL1907 <samuel.lennard@icloud.com>
2022-12-26 01:13:18 +0000

    Fix testing command

--- src/main/java/com/github/sl1907/mineopoly/Mineopoly.java
+++ src/main/java/com/github/sl1907/mineopoly/Mineopoly.java
@@ -6,13 +6,11 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import me.lucko.helper.Commands;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 
 public final class Mineopoly extends JavaPlugin {
@@ -24,34 +22,33 @@ public final class Mineopoly extends JavaPlugin {
         Commands.create()
                 .assertPlayer()
                 .handler(c -> {
-                    URL resource = this.getClass().getResource("london.json");
-                    if (resource != null) {
-                        MineopolySet mineopolySet = this.readJson(resource);
-                        if (mineopolySet == null) {
-                            return;
-                        }
+                    InputStream resource = this.getResource("london.json");
 
-                        for (MineopolyProperty property : mineopolySet.getProperties()) {
-                            c.sender().sendMessage(Component.text(
-                                    "%s | %s %s".formatted(property.getId(), property.getName(), Arrays.toString(property.getRent()))
-                            ));
-                        }
+                    if (resource == null) {
+                        c.sender().sendMessage(Component.text("An error occurred while loading the set.", NamedTextColor.RED));
+                        return;
+                    }
+
+                    MineopolySet mineopolySet = this.readJson(resource);
+                    if (mineopolySet == null) {
+                        c.sender().sendMessage(Component.text("An error occurred.", NamedTextColor.RED));
+                        return;
+                    }
+
+                    c.sender().sendMessage(Component.text("Loaded set %s successfully.".formatted(mineopolySet.getName()), NamedTextColor.GREEN));
+                    for (MineopolyProperty property : mineopolySet.getProperties()) {
+                        c.sender().sendMessage(Component.text(
+                                "%s | %s %s".formatted(property.getId(), property.getName(), Arrays.toString(property.getPos1())),
+                                NamedTextColor.AQUA
+                        ));
                     }
                 })
                 .register("mineopoly");
     }
 
-    private MineopolySet readJson(URL path) {
-        try {
-            File file = new File(path.toURI());
-            try (FileReader fileReader = new FileReader(file)) {
-                return GSON.fromJson(fileReader, MineopolySet.class);
-            } catch (IOException e) {
-                return null;
-            }
-        } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
-        }
+    private MineopolySet readJson(InputStream stream) {
+        InputStreamReader reader = new InputStreamReader(stream);
+        return GSON.fromJson(reader, MineopolySet.class);
     }
 
     @Override
