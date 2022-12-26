8641400e6e91f89750f68740df6ad9b165391c32
SL1907 <samuel.lennard@icloud.com>
2022-12-26 14:55:09 +0000

    Create MineopolyCard tile as it was forgotten before

---
+++ src/main/java/com/github/sl1907/mineopoly/structure/tiles/MineopolyCard.java
@@ -0,0 +1,9 @@
+package com.github.sl1907.mineopoly.structure.tiles;
+
+import com.github.sl1907.mineopoly.structure.MineopolyTile;
+
+public class MineopolyCard extends MineopolyTile {
+
+    // TODO: Add action for land.
+
+}
